f7635c4394d29115a49a78ba86dd0c89a7b2a48f
manis-h <manishabc83@gmail.com>
2023-03-06 14:15:46 +0530

    updated

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import ENV from "../router/connfig.js"
 import { MongoMemoryServer } from "mongodb-memory-server";
 async function connect(){
     const mongod =await MongoMemoryServer.create();
     const getUri = mongod.getUri();
     mongoose.set('strictQuery',true)
-    const db = await mongoose.connect(getUri);
+    // const db = await mongoose.connect(getUri);
+    
+    const db = await mongoose.connect(ENV.ATLAS_URI);
     console.log("DATABASE CONNECTED")
     return db;
 }
--- router/connfig.js
+++ router/connfig.js
@@ -1,5 +1,6 @@
 export default{
     JWT_SECRET :"TXNQzh7dckQ/Kb7WXhw21aYI/cHd7y7VRezaLHeW0Qs=",
     EMAIL :"conner.hudson48@ethereal.email",
-    PASSWORD:"jSdqEtUwGujYyf5nRp"
+    PASSWORD:"jSdqEtUwGujYyf5nRp",
+    ATLAS_URI:"mongodb+srv://manish9958:Manish@cluster0.pq1kopd.mongodb.net/?retryWrites=true&w=majority"
 }

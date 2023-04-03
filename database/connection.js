b9d07e31abc23cd9acd97ecf946e96e97d92c8bb
manis-h <manishabc83@gmail.com>
2023-04-03 14:13:46 +0530

    ok

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import ENV from "../router/connfig.js"
-import { MongoMemoryServer } from "mongodb-memory-server";
+// import { MongoMemoryServer } from "mongodb-memory-server";
 async function connect(){
-    const mongod =await MongoMemoryServer.create();
+    // const mongod =await MongoMemoryServer.create();
     const getUri = mongod.getUri();
     mongoose.set('strictQuery',true)
     // const db = await mongoose.connect(getUri);
